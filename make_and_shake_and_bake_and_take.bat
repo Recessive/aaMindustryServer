37f465a5f2b3b6e60104d157756454d042631355
Recessive <AA.mindustry@gmail.com>
2023-01-16 16:23:37 +1100

    Changed github depolyment stuff to stop trying to push to anukes repos and use mine

--- make_and_shake_and_bake_and_take.bat
+++ make_and_shake_and_bake_and_take.bat
@@ -1,5 +1,5 @@
 @echo off
-CALL gradlew server:dist -Pbuildversion=140
-CALL gradlew desktop:dist -Pbuildversion=140
+CALL gradlew server:dist -Pbuildversion=141.1
+CALL gradlew desktop:dist -Pbuildversion=141.1
 move .\server/build\libs\server-release.jar .\build
 move .\desktop\build\libs\Mindustry.jar .\build
